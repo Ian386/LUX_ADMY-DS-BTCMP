90c855618b14552961b05dbbe1e5dbc7c5ee54c1
Ian Ndolo Mwau <ian.mwau@students.jkuat.ac.ke>
2024-08-04 19:50:09 +0300

    Week1.sql

--- Week1.sql
+++ Week1.sql
@@ -15,3 +15,7 @@ SELECT COUNT(*) FROM weather WHERE weather LIKE '%Snow%';
 
 --Rename the column "Weather" to "Weather_Condition."
 ALTER TABLE weather RENAME COLUMN weather TO Weather_Condition;
+
+--Just a try
+--What is the mean value of each column for each weather condition?
+SELECT Weather_Condition, AVG(Temperature_C) AS Avg_Temperature, AVG(Wind_Speed) AS Avg_Wind_Speed, AVG(Visibility_km) AS Avg_Visibility_km FROM weather GROUP BY Weather_Condition;
